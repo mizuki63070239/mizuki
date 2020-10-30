69b8d411954a3787220184da01abd64bc26c6957
mizuki63070239 <71124555+mizuki63070239@users.noreply.github.com>
2020-10-30 16:17:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <head>
     <meta charset="utf-8">
     <body>
-        <h1>My First Website</p>
+        <h1>My First Website</h1>
         <img src="me.jpg">
         <br>
         <a href="https://www.facebook.com/miura.mi.372//">my contact <Link></a>
 
-        </h3>
+        
         <ul>
             <li>My name is Mizuki.</li>
             <li>My lastname is Miura.</li>
@@ -17,12 +17,11 @@
         </ul>
         <h3>My Joke555555555555</h3>
         <img src="https://scontent.fbkk9-3.fna.fbcdn.net/v/t1.15752-9/122901614_365492451174313_861804521267719447_n.jpg?_nc_cat=105&ccb=2&_nc_sid=ae9488&_nc_eui2=AeHwX7qzrf0D8ZhokdbS3C2FwPXNH1pf34LA9c0fWl_fgtFIM6F0h1wzygm0ijgnFMLwhcuCHV3GMdlyZT_0unJu&_nc_ohc=3wqJpurp6GsAX_OZhYx&_nc_ht=scontent.fbkk9-3.fna&oh=3b31cd1e909cf2f79fee0a807be6abfe&oe=5FC2DA85">
-        <ul>
+        <br>
         <p>ถ้าคุณจะขอสาวแต่งงาน<p>
         <p>รบกวนไปขอกันที่บ้านเถอะครับ<p>
         <p>คุณไม่มีวันรู้หรอก</p>
         <p>ไอ้ผู้ชายเสื้อฟ้าข้างหลัง...เจออะไรบ้าง</p>
         <p>หลังจากวันนั้น...</p>
-        </ul>
     </body>
 </html>
