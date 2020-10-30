930d8baa1887ffed1f2596c4a8498f69b6176f8a
mizuki63070239 <71124555+mizuki63070239@users.noreply.github.com>
2020-10-30 16:15:58 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <li>You can called me Mi.</li>
         </ul>
         <h3>My Joke555555555555</h3>
-        <img src="joke.jpg">
+        <img src="https://scontent.fbkk9-3.fna.fbcdn.net/v/t1.15752-9/122901614_365492451174313_861804521267719447_n.jpg?_nc_cat=105&ccb=2&_nc_sid=ae9488&_nc_eui2=AeHwX7qzrf0D8ZhokdbS3C2FwPXNH1pf34LA9c0fWl_fgtFIM6F0h1wzygm0ijgnFMLwhcuCHV3GMdlyZT_0unJu&_nc_ohc=3wqJpurp6GsAX_OZhYx&_nc_ht=scontent.fbkk9-3.fna&oh=3b31cd1e909cf2f79fee0a807be6abfe&oe=5FC2DA85">
         <ul>
         <p>ถ้าคุณจะขอสาวแต่งงาน<p>
         <p>รบกวนไปขอกันที่บ้านเถอะครับ<p>
