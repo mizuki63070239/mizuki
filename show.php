10c64090df425d2d7ce77756e51abad86e0d5ecb
mizuki63070239 <71124555+mizuki63070239@users.noreply.github.com>
2020-11-27 15:37:46 +0700

    Update show.php

--- show.php
+++ show.php
@@ -11,7 +11,7 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM mi');
 ?>
 <br><br>
 <div class="container">
